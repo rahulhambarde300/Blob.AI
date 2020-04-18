9b4c61ecff074dc3787d019257f4358581e7099a
rahulhambarde300 <rahulhambarde300@gmail.com>
2020-04-18 13:44:26 +0530

    Blob.AI
    1st commit

---
+++ Assets/BlobController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobController : MonoBehaviour
+{
+    public Transform player;
+    public float followRange;
+    Rigidbody rb;
+    Vector2 moveVelocity;
+    bool follow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        follow = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(player.position, transform.position) > followRange && follow)
+        {
+            moveVelocity = player.position - transform.position;
+            moveVelocity *= new Vector2(0.5f, 0.5f);
+            followPlayer();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Goto(Input.mousePosition);
+            follow = follow ? false : true;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Wait();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AutoAttack();
+        }
+    }
+
+    private void followPlayer()
+    {
+        rb.velocity = moveVelocity;
+    }
+
+    private void Goto(Vector3 position)
+    {
+        //Blob will go to the pointed place
+        Vector2 mouse = Input.mousePosition;
+        //rb.MovePosition(Input.mousePosition);
+        //rb.velocity = (Input.mousePosition - transform.position)* new Vector2(0.5f,0.5f);
+
+        while(transform.position != position)
+        {
+            rb.MovePosition(position * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Wait()
+    {
+        //Blob will wait in current position
+    }
+
+    private void AutoAttack()
+    {
+        //Blob will autoattack enemies
+    }
+}
