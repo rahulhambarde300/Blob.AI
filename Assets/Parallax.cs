8c0c133673598e4efeda3e92e8287a6f0261e943
anurag1299 <52789951+anurag1299@users.noreply.github.com>
2020-04-20 23:10:56 +0530

    level done parallax done

---
+++ Assets/Parallax.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallax;
+
+    public void moveBackground(int facingDirection)
+    {
+        transform.position = new Vector3( transform.position.x - facingDirection * Time.deltaTime * parallax,transform.position.y,transform.position.z);
+    }
+}
